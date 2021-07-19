9b13972a202973bcc8b0a93165b10de5cd6b6154
borzenkov-an <borzenkov-an@tion.ru>
2021-07-19 09:31:37 +0700

    изменен тестовый скрипт для конвертирования структуры в байты и обратно

--- emulator/t_3331_pack.py
+++ emulator/t_3331_pack.py
@@ -11,34 +11,121 @@ from collections import namedtuple
 ##	int8_t ReserveArray[16];	//
 ##} t_3331_receive_information_on_device_response; // Receive information on device
 
-UINT8_SIZE = 'b'
-UINT16_SIZE = 'h'
-UINT32_SIZE = ''
 
-##t_3331_ModeOfOperation = 'ModeOfOperation'
-##t_3331_DeviceType      = 'DeviceType'
-##t_3331_SubDeviceType   = 'SubDeviceType'
-##t_3331_SoftwareVertion = 'SoftwareVertion'
-##t_3331_BoardVersion    = 'BoardVersion'
-##t_3331_ReserveArray    = 'ReserveArray'
-##
-##t_3331_keys = (
-##        t_3331_ModeOfOperation,
-##        t_3331_DeviceType,
-##        t_3331_SubDeviceType,
-##        t_3331_SoftwareVertion,
-##        t_3331_BoardVersion,
-##        t_3331_ReserveArray
-##    )
-##
-##t_3331_data_format = {
-##        t_3331_ModeOfOperation   : UINT8_SIZE,
-##        t_3331_DeviceType        : UINT16_SIZE,
-##        t_3331_SubDeviceType     : UINT16_SIZE,
-##        t_3331_SoftwareVertion   : UINT16_SIZE,
-##        t_3331_BoardVersion      : UINT16_SIZE,
-##        t_3331_ReserveArray      : 16
-##    }
+CHAR               = 'c' # size = 1 (bytes of length 1)
+SINED_CHAR         = 'b' # size = 1 (integer)
+UNSIHNED_CHAR      = 'B' # size = 1 (integer)
+_BOOL              = '?' # size = 1 (bool)
+SHORT              = 'h' # size = 2 (integer)
+UNSIGNED_SHORT     = 'H' # size = 2 (integer)
+INT                = 'i' # size = 4 (integer)
+UNSIGNED_INT       = 'I' # size = 4 (integer)
+LONG               = 'l' # size = 4 (integer)
+UNSIGNED_LONG      = 'L' # size = 4 (integer)
+LONG_LONG          = 'q' # size = 8 (integer)
+UNSIGNED_LONG_LONG = 'Q' # size = 8 (integer)
+SSIZE_T            = 'n' # size =  (integer)
+SIZE_T             = 'N' # size =  (integer)
+FLOAT_16BIT        = 'e' # size = 2 (float)
+FLOAT_32BIT        = 'f' # size = 4 (float)
+DOUBLE             = 'd' # size = 8 (float)
+CHAR_ARR_S         = 's' # size =  (bytes)
+CHAR_ARR_P         = 'p' # size =  (bytes)
+VOID_PTR           = 'p' # size =  (integer)
+
+
+INT8_FORMAT  = SINED_CHAR
+INT16_FORMAT = SHORT
+INT32_FORMAT = INT
+
+UINT8_FORMAT  = UNSIHNED_CHAR
+UINT16_FORMAT = UNSIGNED_SHORT
+UINT32_FORMAT = UNSIGNED_LONG
+
+
+##packed_format = INT8_FORMAT + (4*INT16_FORMAT) + ('16'+CHAR_ARR_S)
+##print('packed_format:', packed_format)
+
+ModeOfOperationName = 'ModeOfOperation'
+DeviceTypeName      = 'DeviceType'
+SubDeviceTypeName   = 'SubDeviceType'
+SoftwareVertionName = 'SoftwareVertion'
+BoardVersionName    = 'BoardVersion'
+ReserveArrayName    = 'ReserveArray[16]'
+
+ModeOfOperation = {ModeOfOperationName  : INT8_FORMAT}
+DeviceType      = {DeviceTypeName       : INT16_FORMAT}
+SubDeviceType   = {SubDeviceTypeName    : INT16_FORMAT}
+SoftwareVertion = {SoftwareVertionName  : INT16_FORMAT}
+BoardVersion    = {BoardVersionName     : INT16_FORMAT}
+ReserveArray    = {ReserveArrayName     : INT8_FORMAT}
+
+t_3331_types = {
+        ModeOfOperationName : INT8_FORMAT,
+        DeviceTypeName      : INT16_FORMAT,
+        SubDeviceTypeName   : INT16_FORMAT,
+        SoftwareVertionName : INT16_FORMAT,
+        BoardVersionName    : INT16_FORMAT,
+        ReserveArrayName    : '16' + CHAR_ARR_S
+    }
+
+t_3331_values = {
+        ModeOfOperationName : 0,
+        DeviceTypeName      : 0,
+        SubDeviceTypeName   : 0,
+        SoftwareVertionName : 0,
+        BoardVersionName    : 0,
+        ReserveArrayName    : 0
+        }
+
+STRUCT_DATA_KEY = 'data_key'
+STRUCT_TYPES_KEY = 'types_key'
+
+t_3331_struct = {
+        STRUCT_DATA_KEY  : t_3331_values,
+        STRUCT_TYPES_KEY : t_3331_types
+    }
+##------------------------------------------------------------------------------
+def GetPackedFormat(data_struct):
+    pack_types = data_struct[STRUCT_TYPES_KEY]
+    packed_format = '<'
+    for key in pack_types:
+        packed_format += pack_types[key]
+    return packed_format
+##------------------------------------------------------------------------------
+def UnPack(data_struct, data_bytes):
+    pack_data = data_struct[STRUCT_DATA_KEY]
+    packed_format = GetPackedFormat(data_struct) #'<'
+    unpack_data = unpack(packed_format, data_bytes)
+    i = 0;
+    for key in pack_data:
+        pack_data[key] = unpack_data[i]
+        i += 1
+##------------------------------------------------------------------------------
+def Pack(data_struct):
+    pack_data = data_struct[STRUCT_DATA_KEY]
+    pack_types = data_struct[STRUCT_TYPES_KEY]
+    bytes_data = bytes()
+    for key in pack_data:
+        bytes_data += pack(pack_types[key], pack_data[key])
+        print(pack(pack_types[key], pack_data[key]))
+        print('pack_type:',pack_types[key],'data:',pack_data[key],pack(pack_types[key], pack_data[key]))
+    return bytes_data
+##------------------------------------------------------------------------------
+def StructFill(struct_for_fill):
+    values = struct_for_fill[STRUCT_DATA_KEY]
+    i = 1
+    for key in values.keys():
+        values[key] = i
+        i += 1
+    values[ReserveArrayName] = b'1234567890123456'
+
+StructFill(t_3331_struct)
+data_bytes = Pack(t_3331_struct)
+print(data_bytes)
+UnPack(t_3331_struct, data_bytes)
+print('unpacks:', t_3331_struct)
+##------------------------------------------------------------------------------
 
 packed_format = 'bhhhh16s'
 print('packed_format size: ', calcsize(packed_format), 'bytes')
@@ -93,7 +180,7 @@ Student = namedtuple('Student', student_keys)
 student_tuple = Student._make(unpack('<10sHHb', record))
 
 print('type(student_tuple): ', type(student_tuple))
-print(student_tuple)
+print('student_tuple:',student_tuple)
 
 for st in student_tuple:
     print(type(st), st)
